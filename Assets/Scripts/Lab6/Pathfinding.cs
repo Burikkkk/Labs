3f5fbda08a4eba315118bc879a5439416b9e7b9f
zymxer <ilyayan1756@gmail.com>
2025-03-25 15:51:48 +0100

    dijkstra removed

--- Assets/Scripts/Lab6/Pathfinding.cs
+++ Assets/Scripts/Lab6/Pathfinding.cs
@@ -15,9 +15,8 @@ public class Pathfinding : MonoBehaviour
     private void Start()
     {
         graph = graphGenerator.graph;
-        FindPathDFS();  // хз правильно ли работает по нарисованному пути, было норм, стало хз
+        FindPathDFS();
         FindPathBFS();
-        //FindPathDijkstra(); // я думал, что крутой и еще могу такое, но не
     }
     
     public List<Vertex> FindPathDFS()   // просто находит путь, не особо кратчайший
@@ -66,10 +65,6 @@ public class Pathfinding : MonoBehaviour
 
         }
         path.Reverse();
-        foreach (var v in path)
-        {
-            Debug.Log(v.id);
-        }
         return path;
     }
     
@@ -120,86 +115,6 @@ public class Pathfinding : MonoBehaviour
         }
 
         path.Reverse();
-        // foreach (var v in path)
-        // {
-        //     Debug.Log(v.id);
-        // }
-        return path;
-    }
-
-    public List<Vertex> FindPathDijkstra()  // не работает и запускать в таком виде не надо, зациклится и надо будет перезапускать юнити
-    {
-        Vertex startVertex = graph.GetNearestVertex(transform.position);
-        Vertex endVertex = graph.GetNearestVertex(endPoint.position);
-        
-        List<Vertex> path = new List<Vertex>();
-        
-        SortedSet<Edge> distances = new SortedSet<Edge>();  // это по идее можно вместо бинарной кучи из книги (а можно и без нее и без этого)
-        // тут массив ребер, потому что хранит кратчайшее расстояние от старта до какого-то другого (не важно, что не соседи)
-        
-        bool[] visited = new bool[graph.vertices.Count];
-        visited[startVertex.id] = true;
-        
-        foreach (var neighbour in startVertex.neighbours)
-        {
-            distances.Add(new Edge(neighbour.vertex, neighbour.cost));
-            neighbour.vertex.prev = startVertex;
-        }
-
-        Vertex currentVertex = startVertex;
-        while (currentVertex.id != endVertex.id)
-        {
-            Edge currentEdge = distances.Min;   // всегда начинаем с вершины, до которой идти меньше всего.
-            currentVertex = currentEdge.vertex; // когда в цикле доходим до этой вершины, это значит, что сейчас дошли до нее кратчайшим путем
-
-            if (currentVertex.id == endVertex.id)
-            {
-                break;
-            }
-
-            if (visited[currentVertex.id] == true)
-            {
-                distances.Remove(currentEdge);  // это из-за возможных повторок на строчке 179
-                continue;
-            }
-
-            visited[currentVertex.id] = true;
-            
-            foreach (var neighbour in currentVertex.neighbours)
-            {
-                if (visited[neighbour.vertex.id] == true)
-                {
-                    continue;
-                }
-                float neighbourDistance = currentEdge.cost + neighbour.cost;    // считаем как далеко идти от старта до этого соседа
-                Edge savedEdge;
-                if (distances.TryGetValue(neighbour, out savedEdge))    // если он уже сохранен в том сете, то проверяем, короче ли текущий путь, чем сохраненный
-                {
-                    if (savedEdge.cost > neighbourDistance)
-                    {
-                        savedEdge.vertex.prev = currentVertex;  // если да, то в соседе обновим, что пришли в него из текущей вершины
-                    }
-                }
-                else
-                {
-                    neighbour.vertex.prev = currentVertex;  // если не сохранен, то просто так записваем текущую вершину как предыдущую
-                }
-                distances.Add(new Edge(neighbour.vertex, neighbourDistance));   // может добавить лишнее, но должен скипнуть из-за ифа вверху
-            }
-
-            distances.Remove(currentEdge);  // посмотрели соседей текущей вершины - удаляем
-        }
-        
-        Vertex currentPathVertex = endVertex;   // как везде
-        while (currentPathVertex != startVertex)
-        {
-            if(drawPath)
-                Debug.DrawLine(currentPathVertex.transform.position, 
-                    currentPathVertex.prev.transform.position, Color.yellow, 1000f);
-            path.Add(currentPathVertex);
-            currentPathVertex = currentPathVertex.prev;
-
-        }
         return path;
     }
 }
